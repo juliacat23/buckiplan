ef524b71569634b6b81f1b7dba520556dc90c581
Julia Catalano <catalano.43@osu.edu>
2022-06-21 16:12:44 -0400

    install prettier and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,17 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/typescript/recommended", "plugin:prettier/recommended", "plugin:storybook/recommended"],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
-};
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: ['eslint:recommended', 'plugin:vue/vue3-essential', 'prettier'],
+
+    parserOptions: {
+        ecmaVersion: 2020,
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'vue/require-default-prop': 'off',
+    },
+    plugins: ['prettier'],
+}
---
+++ src/stories/AddCourseButton.stories.ts
@@ -0,0 +1,2 @@
+import AddCourseButton from '@/components/AddCourseButton.vue';
+import { Meta } from '@sotyrbook/vue3';
