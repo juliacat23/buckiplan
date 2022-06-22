0727af74e5a0645eadb5739e0279e79d98e896fc
Julia Catalano <catalano.43@osu.edu>
2022-06-22 09:36:48 -0400

    remove no-undef error from eslint

--- src/firebase/usernameData.ts
+++ src/firebase/usernameData.ts
@@ -2,11 +2,7 @@ import { usernameCollection } from '@/config/fbConfig';
 import store from '@/store';
 
 // todo: why is it not recognizing interface from other file
-type FirestoreUserName = {
-    readonly firstName: string;
-    readonly middleName?: string;
-    readonly lastName: string;
-};
+
 
 const setUsernameData = (name: FirestoreUserName): void => {
     usernameCollection.doc(store.state.currentFirebaseUser.email).set({
