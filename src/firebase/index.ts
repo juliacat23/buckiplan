7353d21cb091c4e634f0bd5ad9476f821d862bcc
Julia Catalano <catalano.43@osu.edu>
2022-06-22 17:06:33 -0400

    export firestore functions

---
+++ src/firebase/index.ts
@@ -0,0 +1,27 @@
+import store from '@/store';
+import { incrementUniqueID } from './utils';
+import { osuCourseToFirebaseSemesterCourse } from '@/userDataConverter';
+
+export const osuCourseToFirebaseSemesterCourseWithGlobalData = (course: OSUCourse): FirestoreSemesterCourse =>
+  osuCourseToFirebaseSemesterCourse(course, store, incrementUniqueID);
+
+export { incrementUniqueID };
+export { setAppOnboardingData, deleteTransferCredit } from './onboardingData';
+export { default as chooseToggleableRequirementOption } from './toggeableRequirementChoices';
+export {
+  updateRequirementChoice,
+  toggleRequirementChoice,
+  updateRequirementChoices,
+  deleteCourseFromRequirementChoices,
+} from './overrideFulfillment';
+export {
+  editSemester,
+  editSemesters,
+  addSemester,
+  deleteSemester,
+  addCourseToSemester,
+  deleteCourseFromSemester,
+  deleteAllCoursesFromSemester,
+  deleteCourseFromSemesters,
+  populateSemesters,
+} from './semesters';
