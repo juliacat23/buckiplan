5d1aa451e63a17aecfe1211284e53b7d508b7832
Julia Catalano <catalano.43@osu.edu>
2022-06-27 18:01:38 -0400

    download fonts and set as default style in app

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import VueGtag from 'vue-gtag-next';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'intro.js/introjs.css';
+
 import './scss/app.scss';
+import './scss/introjs.scss';
 
 import App from './App.vue';
 import router from './router';
