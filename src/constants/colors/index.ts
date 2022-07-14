19a3d18d17de165e843423c7be7b4c0a6165e0d6
Julia Catalano <catalano.43@osu.edu>
2022-07-14 15:48:58 -0400

    update colors

--- src/constants/colors/index.ts
+++ src/constants/colors/index.ts
@@ -14,8 +14,16 @@ export const coursesColorSet = [
     hex: '#3cb44b',
   },
   {
+    text: 'Amythest',
+    hex: '#9966CC',
+  },
+  {
     text: 'Blue',
     hex: '#4169E1',
+  },
+  {
+    text: 'Watermelon',
+    hex: '#fc6c85',
   },
   {
     text: 'Orange',
@@ -30,12 +38,20 @@ export const coursesColorSet = [
     hex: '#3bbfdc',
   },
   {
+    text: 'Electric',
+    hex: '#0592D0',
+  },
+  {
     text: 'Magenta',
     hex: '#f032e6',
   },
   {
     text: 'Lime',
     hex: '#acd73e',
+  },
+  {
+    text: 'Burnt Sienna',
+    hex: '#e97451',
   },
   {
     text: 'Pink',
@@ -46,16 +62,24 @@ export const coursesColorSet = [
     hex: '#469990',
   },
   {
+    text: 'Cadet Blue',
+    hex: '#5f9ea0',
+  },
+  {
     text: 'Lavender',
     hex: '#dcbeff',
   },
   {
-    text: 'Brown',
-    hex: '#9A6324',
+    text: 'Marigold',
+    hex: '#eba832',
   },
   {
     text: 'Light Coral',
     hex: '#F08080',
+  },
+  {
+    text: 'Purple Gray',
+    hex: '#a7a6ba',
   },
   {
     text: 'Plum',
@@ -66,12 +90,24 @@ export const coursesColorSet = [
     hex: '#88cc9c',
   },
   {
+    text: 'Rogue',
+    hex: '#a94064',
+  },
+  {
     text: 'Salmon',
     hex: '#FFA07A',
   },
   {
     text: 'Cobalt',
     hex: '#1E90FF',
+  },
+  {
+    text: 'Ash Gray',
+    hex: '#b2beb5',
+  },
+  {
+    text: 'Dark Orange',
+    hex: '#ee7600',
   },
   {
     text: 'Navy',
@@ -132,5 +168,9 @@ export const coursesColorSet = [
   {
     text: 'Light Slate Gray',
     hex: '#778899',
+  },
+  {
+    text: 'Rose Quartz',
+    hex: '#f7cac9',
   },
 ];
